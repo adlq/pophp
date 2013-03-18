f336440aee1f77a12848fb69fe3da5088463e756
Nghia D <tnghia.duong@gmail.com>
2013-03-18 18:28:31 +0100

    Full parsing
    Still need refactoring

--- POParser.php
+++ POParser.php
@@ -1,17 +1,132 @@
 <?php
 
-$po = new POFile("lang.po");
-
 class POFile 
 {
 	private $entries;
 	
 	public function __construct($file)
 	{
-		$entries = array();
-		POParser::parse($file);
+		$this->entries = POParser::parse($file);
+	}
+
+    public function getEntries($files = array())
+    {
+		$result = array();
+		if (!empty($files)) 
+		{
+			foreach ($this->entries as $entry) 
+			{
+				$comments = $entry->getComments();
+				if (array_key_exists("reference", $comments))
+				{
+					// Retrieve the referenced file path
+					foreach ($comments["reference"] as $reference)
+					{
+						if (preg_match("/(.+):/", $reference, $match))
+						{
+							$referencePath = $match[1];
+							if (in_array($referencePath, $files))
+							{
+								array_push($result, $entry);
+								break;
+							}
+						}
+					}
+				}
+			}
+			return $result;
+		}
+        return $this->entries;
+    }
+
+    public function display($entries = array())
+    {
+		$entries = empty($entries) ? $this->entries : $entries;
+        foreach ($this->entries as $entry)
+        {
+            // Display comments first
+            $comments = $entry->getComments();            
+            foreach ($comments as $type => $comment) 
+            {
+                // Display comments
+                switch ($type)
+                {
+                    case "translator":
+                        foreach ($comment as $translatorComment)
+                        {
+                            echo "# $translatorComment\n";
+                        }
+                        break;
+                    case "extracted":
+                        echo "#. $comment\n";
+                        break;
+                    case "reference":
+                        foreach ($comment as $ref)
+                        {
+                            echo "#: $ref\n";
+                        }
+                        break;
+                    case "flag":
+                        echo "#, $comment\n";
+                        break;
+                    default:
+                        echo "#| $comment\n";
+                        break;
+                }
+            }
+
+            // Context
+            $context = $entry->getContext();
+            if ($context != "") 
+                echo "msgctxt \"" . $context . "\"\n";
+
+            // msgid
+            $source = $entry->getSource();
+            $this->displayWithLineBreak($source);
+
+            // msgstr 
+            $target = $entry->getTarget();
+            $this->displayWithLineBreak($target);
+
+            echo "\n";
+        }
+    }
+
+    private function displayWithLineBreak($str)
+    {
+	
+        $offset = 0;
+        $break = strpos($str, '\n', $offset) != false;
+        echo "msgstr ";
+        if ($break == false)
+        {
+            echo "\"$str\"\n";
+        }
+        else 
+        {
+            
+            while ($break != false)
+            {
+                $break = strpos($str, '\n', $offset);
+                echo "\"" . substr($str, $offset, $break - $offset) . '\n' . "\"\n";
+                $padding = strlen('\n');
+                $offset = $break + $padding > strlen($str) ? strlen($target) - 1 : $break + $padding; 
+            }
+        }
+    } 
+	
+	public function getSourceStrings($files = array())
+	{
+		$entries = $this->getEntries($files);
+		$sourceStrings = array();
+		foreach ($entries as $entry) 
+		{
+			array_push($sourceStrings, $entry->getSource());
+		}
+		return $sourceStrings;
 	}
 }
+
 
 class POEntry 
 {
@@ -27,6 +142,26 @@ class POEntry
 		$this->source = $source;
 		$this->target = $target;
 	}	
+
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    public function getTarget()
+    {
+        return $this->target;
+    }
 }
 
 class POParser
@@ -38,9 +173,9 @@ class POParser
 		$regexes = array(
 		"comment" 	=> 	"/^#(.+?)\n/",
 		"quote"		=>	"/^\"(.+?)\"/",
-		"msgctxt" 	=>	"/^msgctxt \"(.+?)?\"/",
-		"msgid" 	=>	"/^msgid \"(.+?)?\"/",
-		"msgstr" 	=>	"/^msgstr \"(.+?)?\"/");
+		"msgctxt" 	=>	"/^msgctxt \"(.*)?\"/",
+		"msgid" 	=>	"/^msgid \"(.*)?\"/",
+		"msgstr" 	=>	"/^msgstr \"(.*)?\"/");
 	
 		$lines = file($file);
 		
@@ -59,16 +194,16 @@ class POParser
 				switch ($match[1][0])
 				{
 					case ".":
-						$comments["extracted"] = substr($match[1], 2);
+						$comments["extracted"][] = substr($match[1], 2);
 						break;
 					case ":":
-						$comments["reference"] = substr($match[1], 2);
+						$comments["reference"][] = str_replace('\\', '/', substr($match[1], 2));
 						break;
 					case ",":
-						$comments["flag"] = substr($match[1], 2);
+						$comments["flag"][] = substr($match[1], 2);
 						break;
 					case " ":
-						$comments["translator"] = substr($match[1], 1);
+						$comments["translator"][] = substr($match[1], 1);
 						break;
 					case "|":
 						$attr = array();
@@ -124,40 +259,8 @@ class POParser
 				}
 			}
 		}
-		array_push($entries, new POEntry($buffers["source"], $buffers["target"]));
-		
-		print_r($entries);
+		array_push($entries, new POEntry($buffers["source"], $buffers["target"], $buffers["context"], $comments));
+        return $entries;
 	}
 }
-
-abstract class ParserState
-{
-	
-}
-
-class CommentState extends ParserState
-{
-
-}
-
-class MsgIdState extends ParserState
-{
-
-}
-
-class MsgStrState extends ParserState
-{
-
-}
-
-class BlankLineState extends ParserState
-{
-
-}
-
-class Context 
-{
-
-}
-
 ?>
