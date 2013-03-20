935334150af0f2c28de79b68271339ea478aea66
adlq <dtnghia90@gmail.com>
2013-03-20 10:30:38 +0100

    Add sample unit tests

--- POParser.php
+++ POParser.php
@@ -1,169 +1,4 @@
 <?php
-
-class POFile 
-{
-	private $entries;
-	
-	public function __construct($file)
-	{
-		$this->entries = POParser::parse($file);
-	}
-
-    public function getEntries($files = array())
-    {
-		$result = array();
-		if (!empty($files)) 
-		{
-			foreach ($this->entries as $entry) 
-			{
-				$comments = $entry->getComments();
-				if (array_key_exists("reference", $comments))
-				{
-					// Retrieve the referenced file path
-					foreach ($comments["reference"] as $reference)
-					{
-						if (preg_match("/(.+):/", $reference, $match))
-						{
-							$referencePath = $match[1];
-							if (in_array($referencePath, $files))
-							{
-								array_push($result, $entry);
-								break;
-							}
-						}
-					}
-				}
-			}
-			return $result;
-		}
-        return $this->entries;
-    }
-
-    public function display($entries = array())
-    {
-		$entries = empty($entries) ? $this->entries : $entries;
-        foreach ($this->entries as $entry)
-        {
-            // Display comments first
-            $comments = $entry->getComments();            
-            foreach ($comments as $type => $comment) 
-            {
-                // Display comments
-                switch ($type)
-                {
-                    case "translator":
-                        foreach ($comment as $translatorComment)
-                        {
-                            echo "# $translatorComment\n";
-                        }
-                        break;
-                    case "extracted":
-                        echo "#. $comment\n";
-                        break;
-                    case "reference":
-                        foreach ($comment as $ref)
-                        {
-                            echo "#: $ref\n";
-                        }
-                        break;
-                    case "flag":
-                        echo "#, $comment\n";
-                        break;
-                    default:
-                        echo "#| $comment\n";
-                        break;
-                }
-            }
-
-            // Context
-            $context = $entry->getContext();
-            if ($context != "") 
-                echo "msgctxt \"" . $context . "\"\n";
-
-            // msgid
-            $source = $entry->getSource();
-            $this->displayWithLineBreak($source);
-
-            // msgstr 
-            $target = $entry->getTarget();
-            $this->displayWithLineBreak($target);
-
-            echo "\n";
-        }
-    }
-
-    private function displayWithLineBreak($str)
-    {
-	
-        $offset = 0;
-        $break = strpos($str, '\n', $offset) != false;
-        echo "msgstr ";
-        if ($break == false)
-        {
-            echo "\"$str\"\n";
-        }
-        else 
-        {
-            
-            while ($break != false)
-            {
-                $break = strpos($str, '\n', $offset);
-                echo "\"" . substr($str, $offset, $break - $offset) . '\n' . "\"\n";
-                $padding = strlen('\n');
-                $offset = $break + $padding > strlen($str) ? strlen($target) - 1 : $break + $padding; 
-            }
-        }
-    } 
-	
-	public function getSourceStrings($files = array())
-	{
-		$entries = $this->getEntries($files);
-		$sourceStrings = array();
-		foreach ($entries as $entry) 
-		{
-			array_push($sourceStrings, $entry->getSource());
-		}
-		return $sourceStrings;
-	}
-}
-
-
-class POEntry 
-{
-	private $comments;
-	private $context;
-	private $source;
-	private $target;
-	
-	public function __construct($source, $target, $context = "", $comments = array())
-	{
-		$this->comments = $comments;
-		$this->context = $context;
-		$this->source = $source;
-		$this->target = $target;
-	}	
-
-    public function getComments()
-    {
-        return $this->comments;
-    }
-
-    public function getContext()
-    {
-        return $this->context;
-    }
-
-    public function getSource()
-    {
-        return $this->source;
-    }
-
-    public function getTarget()
-    {
-        return $this->target;
-    }
-}
-
 class POParser
 {
 	private $state;
@@ -189,31 +24,38 @@ class POParser
 		{
 			$match = array();
 			
-			if (preg_match($regexes["comment"], $line, $match)) 
+			if (preg_match($regexes["comment"], $line, $match) && $state != "obsolete") 
 			{
 				switch ($match[1][0])
 				{
 					case ".":
-						$comments["extracted"][] = substr($match[1], 2);
+						$comments["extracted"][] = trim(substr($match[1], 2));
 						break;
 					case ":":
-						$comments["reference"][] = str_replace('\\', '/', substr($match[1], 2));
+						$comments["reference"][] = trim(str_replace('\\', '/', substr($match[1], 2)));
 						break;
 					case ",":
-						$comments["flag"][] = substr($match[1], 2);
+						$flags = explode(", ", substr($match[1], 2));
+						foreach ($flags as $flag)
+						{
+							$comments["flag"][] = trim($flag);
+						}
 						break;
 					case " ":
-						$comments["translator"][] = substr($match[1], 1);
+						$comments["translator"][] = trim(substr($match[1], 1));
 						break;
 					case "|":
 						$attr = array();
 						preg_match("/(\w+)/", $match[1], $attr);
 						$pos = strpos($match[1], "\"");
-						$comments["old" . ucfirst($attr[0])] = substr($match[1], $pos);
+						$comments["old" . ucfirst($attr[0])] = trim(substr($match[1], $pos));
+						break;
+					case "~":
+						$state = "obsolete";
 						break;
 				}
 			}
-			else if (preg_match($regexes["msgctxt"], $line, $match))
+			else if (preg_match($regexes["msgctxt"], $line, $match) && $state != "obsolete")
 			{
 				$state = "msgctxt";
 				if (isset($match[1]))
@@ -221,7 +63,7 @@ class POParser
 					$buffers["context"] .= $match[1];
 				}
 			} 
-			else if (preg_match($regexes["msgid"], $line, $match))
+			else if (preg_match($regexes["msgid"], $line, $match) && $state != "obsolete")
 			{
 				$state = "msgid";
 				if (isset($match[1]))
@@ -229,7 +71,7 @@ class POParser
 					$buffers["source"] .= $match[1];
 				}
 			} 
-			else if (preg_match($regexes["msgstr"], $line, $match))
+			else if (preg_match($regexes["msgstr"], $line, $match) && $state != "obsolete")
 			{
 				$state = "msgstr";
 				if (isset($match[1]))
@@ -237,7 +79,7 @@ class POParser
 					$buffers["target"] .= $match[1];
 				}
 			} 
-			else if (preg_match($regexes["quote"], $line, $match))
+			else if (preg_match($regexes["quote"], $line, $match) && $state != "obsolete")
 			{
 				if ($state == "msgid")
 				{
