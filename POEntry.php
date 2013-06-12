7f4612b0ec6caa1e996894687e29016559baea8a
Nghia D <tnghia.duong@gmail.com>
2013-06-12 13:06:39 +0200

    Add new class for PO utility

--- POEntry.php
+++ POEntry.php
@@ -253,5 +253,3 @@ class POEntry
 		return $this->hash;
 	}
 }
-
-?>
--- podiff.php
+++ podiff.php
@@ -1,50 +1,25 @@
 <?php
-require_once("POFile.php");
-require_once("POEntry.php");
+require_once("POUtils.php");
 
 $file1 = $argv[1];
 $file2 = $argv[2];
 
-$pofile1 = new POFile($file1);
-$pofile2 = new POFile($file2);
+$utils = new POUtils();
 
-// Compare both string arrays and retrieve the missing strings as well as the obsolete ones
-$strings1not2 = compareEntries($pofile2, $pofile1);
-$strings2not1 = compareEntries($pofile1, $pofile2);
+$diff = $utils->compare($file1, $file2);
 
-displayStats("# of strings in $file1", count($pofile1->getEntries()));
-displayStats("# of strings in $file2", count($pofile2->getEntries()));
+displayStats("# of strings in $file1", $diff['firstMsgCount']);
+displayStats("# of strings in $file2", $diff['secondMsgCount']);
 
-$strings1not2Count = count($strings1not2);
-$strings2not1Count = count($strings2not1);
+$strings1not2Count = count($diff['firstOnly']);
+$strings2not1Count = count($diff['secondOnly']);
 
 // Output different things 
 displayStats("In $file1 but not in $file2", $strings1not2Count);
-displayEntryArray($strings1not2);
+displayEntryArray($diff['firstOnly']);
 
 displayStats("In $file2 but not in $file1", $strings2not1Count);
-displayEntryArray($strings2not1);
-
-/**
- * Take one reference array and another array to compare to,
- * output their difference.
- *
- * @param	$refArray	the reference array
- *			$array		the array to compare it to
- * @return	the strings from $array that are not included in $refArray
- */
-function compareEntries($file1, $file2)
-{
-	$diffArray = array();
-	
-	foreach ($file1->getEntries() as $entry) 
-	{
-		if (!$file2->getEntry($entry))
-			array_push($diffArray, $entry);
-	}
-	
-	return $diffArray;
-}
+displayEntryArray($diff['secondOnly']);
 
 /**
  * Appropriately display a string array in TortoiseHg's output log
@@ -70,4 +45,3 @@ function displayStats($msg, $stat)
 {
 	echo "\n$msg: $stat\n\n-----\n\n";
 }
-?>
